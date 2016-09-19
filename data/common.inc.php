f961031be3db3552ab68a1f7abff6614ea1342c1
buuug7 <youpp@126.com>
2016-09-19 15:08:51 +0800

    some fixer

--- data/common.inc.php
+++ data/common.inc.php
@@ -1,9 +1,9 @@
 <?php
 //数据库连接信息
 $cfg_dbhost = 'localhost';
-$cfg_dbname = 'tongliao';
+$cfg_dbname = 'dede-tongliao';
 $cfg_dbuser = 'root';
-$cfg_dbpwd = 'root';
+$cfg_dbpwd = 'gansutianqi2016';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
 

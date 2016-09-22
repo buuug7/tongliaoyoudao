50241f72920cad3747f690593f9348b4c7776dcc
buuug7 <youpp@126.com>
2016-09-22 18:22:28 +0800

    修改index.html

--- skin/js/app.js
+++ skin/js/app.js
@@ -2,7 +2,7 @@
  * Created by buuug7 on 2016/9/20.
  */
 $(document).ready(function () {
-
+  $('.bannerBox .cur').hide();
   $(".b7-in").slide({});
 
   //new Marquee("faq", 0, 1, 306, 392, 40, 0, 1000, 22);
